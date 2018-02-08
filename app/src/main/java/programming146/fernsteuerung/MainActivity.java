6d3f9dc00df93d3af22f35ec45dd318ab1084fe0
programmingflo <flori08mans@gmail.com>
2018-02-08 02:25:09 +0100

    add settings menu button and policity for internet connection at main activity

--- app/src/main/java/programming146/fernsteuerung/MainActivity.java
+++ app/src/main/java/programming146/fernsteuerung/MainActivity.java
@@ -1,7 +1,11 @@
 package programming146.fernsteuerung;
 
+import android.content.Intent;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         final EditText commandInput = (EditText) findViewById(R.id.main_command_input);
         TextView commandLog = (TextView) findViewById(R.id.main_command_log);
@@ -60,8 +67,33 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String command = commandInput.getText().toString();
-
+                String[] request = new String[]{"setCommand","keyboard",command};
+                ServerConnection serverConnection = new ServerConnection(getApplicationContext());
+                serverConnection.doInBackground(request);
             }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                Intent settingsIntent = new Intent(getApplicationContext(), SettingsActivity.class);
+                startActivity(settingsIntent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }
