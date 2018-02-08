a44913193d901f86a18070f3c3e0a7db26da5d57
programmingflo <flori08mans@gmail.com>
2018-02-08 02:20:52 +0100

    initial commit

---
+++ app/src/main/java/programming146/fernsteuerung/SettingsActivity.java
@@ -0,0 +1,11 @@
+package programming146.fernsteuerung;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by FM on 07.02.2018.
+ * set settings for command creation and server connection
+ */
+
+public class SettingsActivity extends AppCompatActivity {
+}
